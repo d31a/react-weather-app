55041ca4ba86064dc72a84c86b139e3fd225d5ba
Ben Bagley <benbagleydesign@gmail.com>
2023-01-31 19:54:04 +0000

    updating API call to refresh

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import '../App.css'
 import './styles.css'
-import moment from 'moment'; // package imports the date 
-import icons from 'open-weather-icons'
-// pulls all the info from the API, presents into a card 
-
+//import moment from 'moment'; // package imports the date 
 //    <p className="date"> {moment().format('LL')}</p> pulls date
 
-
+// pulls all the info from the API, present into a card 
     const CardExampleCard = ({weatherData}) => {
       // to add an icon 
-      
       const iconcode = weatherData.weather[0].icon; 
       const iconurl = "http://openweathermap.org/img/wn/" + iconcode + "@4x.png";
+      // rounds value of temp to nearest int 
+      const temp = Math.round(weatherData.main.temp);
+      // add refresh button 
+      const refresh = () => {
+        window.location.reload();
+      }
 
       return (
         <div>
           <div className="card">
-              <div className="header"> {weatherData.name}
+              <div className="header"> 
+              {weatherData.name} <span id="country">{weatherData.sys.country}</span>
               </div>
                 <div className="content">
                 <img id="wicon" src={iconurl} alt="" />
-                <p id="temp">{weatherData.main.temp}&deg;c</p>   
-                <p id="current">Currently {weatherData.weather[0].main} outside</>
+                <p id="temp">{temp}&deg;c</p>   
+                <p id="current">Currently {weatherData.weather[0].main} outside</p>
               </div>
           </div>
         </div>
