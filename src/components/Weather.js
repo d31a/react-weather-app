7d7737d4dfca4fb0f49a2d1c063e742c07bebf7b
Ben Bagley <benbagleydesign@gmail.com>
2023-03-04 18:05:06 +0000

    css

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -10,8 +10,10 @@ import './styles.css'
       const iconcode = weatherData.weather[0].icon; 
       const iconurl = "http://openweathermap.org/img/wn/" + iconcode + "@4x.png";
       // rounds value of temp to nearest int 
-      const temp = Math.round(weatherData.main.temp);
+     const temp = Math.round(weatherData.main.temp);
+
     
+      
       /* add refresh button 
       const refresh = () => {
         window.location.reload();
@@ -23,11 +25,14 @@ import './styles.css'
         <div>
           <div className="card">
               <div className="header"> 
-              {weatherData.name} <span id="country">{weatherData.sys.country}</span>
+              <p className="temp">{temp}<sup>c</sup></p> 
+              <div className="location">
+              <span id="place">{weatherData.name}</span> 
+              <span id="country">{weatherData.sys.country}</span>
+              </div>
               </div>              
                 <div className="content">
                 <img id="wicon" src={iconurl} alt="" />
-                <p className="temp">{temp}&deg;c</p> 
                 <p id="current">Currently {weatherData.weather[0].main} outside</p>
               </div>
           </div>
